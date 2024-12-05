374f47718f5209f7f3e5613d582ee7b8228657b0
Faruk Fuçucuoğlu <farukfucucuoglu@example.com>
2024-12-05 19:36:28 +0300

    Proje güncelleniyor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
+
+import { useState } from 'react';
+import "as math from 'mathjs"
 import './App.css';
 import Button from './components/Button';
-function App(){
+import Input from './components/Input';
+
+function App(){ 
+  const [text,setText] = useState("")
+  const [result,setResult] = useState("");
+
+const addtotext = (val)=> {
+  setText((text)=> [...text,val+" "])
+}
+const resetInput = ()=>{
+  setText("");
+  setResult("");
+
+}
   return(
     <div className="App">
       <div className='calc-wrapper'>
-        <Button />
+        <Input text={text} result={result} />
+        <div className='row'>
+        <Button symbol="7" handeClick={addtotext}/>
+        <Button symbol="8"handeClick={addtotext}/>
+        <Button symbol="9"handeClick={addtotext}/>
+        <Button symbol="/"color="#00AD85"handeClick={addtotext}/>
+        </div>
+        <div className='row'>
+        <Button symbol="4"handeClick={addtotext}/>
+        <Button symbol="5"handeClick={addtotext}/>
+        <Button symbol="6"handeClick={addtotext}/>
+        <Button symbol="*"color="#00AD85"handeClick={addtotext}/>
+        </div>
+        <div className='row'>
+        <Button symbol="1"handeClick={addtotext}/>
+        <Button symbol="2"handeClick={addtotext}/>
+        <Button symbol="3"handeClick={addtotext}/>
+        <Button symbol="+"color="#00AD85"handeClick={addtotext}/>
+        </div>
+        <div className='row'>
+        <Button symbol="0"handeClick={addtotext}/>
+        <Button symbol="."handeClick={addtotext}/>
+        <Button symbol="="/>
+        <Button symbol="-"color="#00AD85"handeClick={addtotext}/>
+        </div>
+        <Button symbol="Clear"color="red"handeClick={resetInput}/>
+        </div>
       </div>
-    </div>
+    
+    
   );
 }
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import './Button.css';
-function Button() {
-  
-   return <div className='button-wrapper'>Test</div>;
-  
-}
+import React from 'react'
+import './Button.css'
 
-export default Button;
+
+function Button({symbol,color,handeClick}) {
+    return(
+        <div 
+        onClick={()=>handeClick(symbol)} 
+        className='button-wrapper'style={{backgroundColor:color}}>{symbol}</div>
+    )
+}
+export default Button
